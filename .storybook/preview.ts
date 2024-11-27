2360ed2850c6261bd8c864265e911e39335e9505
山本勇太 <yamamotoyuuta@yamamotoyuutanoMacBook-Pro.local>
2024-11-27 09:47:46 +0900

    fix preveinstall

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,17 @@
 import type { Preview } from "@storybook/react";
 
+import { addLocale, locale } from "primereact/api";
+import { LocaleJp } from "../src/app/utils/LocaleJp";
+
+// import "../app/styles/global.css";
+import "primereact/resources/themes/saga-blue/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+import "primeflex/primeflex.css";
+
+addLocale("jp", LocaleJp);
+locale("jp");
+
 const preview: Preview = {
   tags: ["autodocs"],
   parameters: {
