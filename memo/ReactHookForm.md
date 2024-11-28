c8a433b62c05789097e2c874a84ac114e76e0108
山本勇太 <yamamotoyuuta@yamamotoyuutanoMacBook-Pro.local>
2024-11-28 21:42:14 +0900

    add react-hook-form yup

---
+++ memo/ReactHookForm.md
@@ -0,0 +1,3 @@
+npm install react-hook-form
+
+npm install yup @hookform/resolvers
--- src/app/(page)/register/page.tsx
+++ src/app/(page)/register/page.tsx
@@ -3,11 +3,15 @@
 import MaskedCalendar from "@/app/components/molecules/maskedCalendar/MaskedCalendar";
 import { COLUMN_POSITIONS, Grid, GridItem } from "@/styles/grid/Grid";
 import { Affiliation } from "@/types/affiliation/Affiliation";
+import { useQuery } from "@tanstack/react-query";
 import { Button } from "primereact/button";
 import { Card } from "primereact/card";
 import { InputText } from "primereact/inputtext";
 import { Steps } from "primereact/steps";
 import { useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 const items = [
   {
@@ -20,21 +24,65 @@ const items = [
     label: "登録完了",
   },
 ];
+type FormValues = {
+  username: string;
+  name: string;
+};
+
+const fetchAffiliations = async (): Promise<Affiliation[]> => {
+  const response = await fetch("/api/affiliations");
+  if (!response.ok) {
+    throw new Error("データの取得に失敗しました");
+  }
+  return response.json();
+};
 
 export default function RegisterPage() {
-  const [affiliations, setAffiliations] = useState<Affiliation[]>([]);
-  const onClickAffiliations = async () => {
-    try {
-      const response = await fetch("/api/affiliations");
-      if (!response.ok) {
-        throw new Error("データの取得に失敗しました");
-      }
-      const data = await response.json();
-      setAffiliations(data);
-    } catch (error) {
-      console.error(error);
-    } finally {
-    }
+  const [isButtonClicked, setIsButtonClicked] = useState(false);
+
+  // 初期データを取得
+  const {
+    data: affiliations,
+    error,
+    isLoading,
+  } = useQuery<Affiliation[], Error>({
+    queryKey: ["affiliations"], // キャッシュキー
+    queryFn: fetchAffiliations, // データ取得関数
+    enabled: isButtonClicked, // ボタンがクリックされたときにのみ実行
+    staleTime: 5 * 60 * 1000, // 5分間はデータを再フェッチしない
+  });
+
+  const onClickAffiliations = () => {
+    setIsButtonClicked(true); // ボタンクリック時にデータ取得を有効にする
+  };
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error instanceof Error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  // バリデーションスキーマ
+  const schema = yup.object().shape({
+    username: yup
+      .string()
+      .required("ユーザー名は必須です")
+      .min(3, "ユーザー名は3文字以上である必要があります"),
+    name: yup.string().required("名称は必須です"),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>({
+    resolver: yupResolver(schema), // yupを適用
+  });
+
+  const onSubmit: SubmitHandler<FormValues> = (data) => {
+    console.log(data);
   };
 
   return (
@@ -42,19 +90,29 @@ export default function RegisterPage() {
       <div className="card">
         <Steps model={items} activeIndex={0} />
       </div>
-      <div style={styles.container}>
-        <Card
-          title="新規会員登録"
-          footer={<Button type="submit">登録</Button>}
-          // style={{ width: "60%" }}
-        >
-          <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div style={styles.container}>
+          <Card
+            title="新規会員登録"
+            footer={<Button type="submit">登録</Button>}
+            style={{ width: "85%" }}
+          >
             <Grid>
               <GridItem $isLabel={true}>
                 <p>ユーザー名:</p>
               </GridItem>
               <GridItem $isLabel={false}>
-                <InputText type="text" name="username" />
+                <div className="w-full">
+                  <InputText
+                    type="text"
+                    {...register("username", {
+                      required: "ユーザー名は必須です",
+                    })}
+                  />
+                  {errors.username && (
+                    <p style={{ color: "red" }}>{errors.username.message}</p>
+                  )}
+                </div>
               </GridItem>
               <GridItem
                 $isLabel={true}
@@ -66,7 +124,15 @@ export default function RegisterPage() {
                 $isLabel={false}
                 $column={COLUMN_POSITIONS.pc.rightInputWidth}
               >
-                <InputText type="text" />
+                <div className="w-full">
+                  <InputText
+                    type="text"
+                    {...register("name", { required: "名称は必須です" })}
+                  />
+                  {errors.name && (
+                    <p style={{ color: "red" }}>{errors.name.message}</p>
+                  )}
+                </div>
               </GridItem>
             </Grid>
             <Grid>
@@ -100,14 +166,18 @@ export default function RegisterPage() {
                 </div>
               </GridItem>
             </Grid>
-            {affiliations.map((item) => (
-              <p key={item.id}>
-                {item.id}/{item.name}
-              </p>
-            ))}
-          </form>
-        </Card>
-      </div>
+            {affiliations && (
+              <div>
+                {affiliations.map((item) => (
+                  <p key={item.id}>
+                    {item.id} / {item.name}
+                  </p>
+                ))}
+              </div>
+            )}
+          </Card>
+        </div>
+      </form>
     </>
   );
 }
