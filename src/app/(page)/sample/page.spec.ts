f43034578276d45853a8e0e96fe7cc99fdb82f17
山本勇太 <yamamotoyuuta@yamamotoyuutanoMacBook-Pro.local>
2024-12-10 21:35:46 +0900

    add testCase

--- src/app/(page)/sample/page.spec.ts
+++ src/app/(page)/sample/page.spec.ts
@@ -1,10 +1,22 @@
 import { test, expect } from "@playwright/test";
 
-test("has title", async ({ page }) => {
+test("toppage title", async ({ page }) => {
   await page.goto("http://localhost:3000");
 
   const h1Text = await page.textContent("h1.text-center.mb-4");
 
   // 値を検証
   expect(h1Text).toBe("Next.js OAuth Example");
+});
+
+test("should check if the element exists", async ({ page }) => {
+  // ページを開く
+  await page.goto("http://localhost:3000");
+
+  // リンクをクリック
+  await page.getByRole("link", { name: "認証が不要な画面" }).click();
+
+  // 要素の存在を確認
+  const targetElement = await page.locator("div.sc-gtLWhw.eARkff > p");
+  await expect(targetElement).toHaveText("組織１組織１組織１組織１");
 });
