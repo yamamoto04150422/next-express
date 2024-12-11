1dfaca5aacab0452b61715252368f4cda5f74758
山本勇太 <yamamotoyuuta@yamamotoyuutanoMacBook-Pro.local>
2024-12-11 22:08:43 +0900

    update header
    router.backを追加

--- src/app/components/organisms/navigationMegaMenu/NavigationMegaMenu.tsx
+++ src/app/components/organisms/navigationMegaMenu/NavigationMegaMenu.tsx
@@ -3,12 +3,31 @@
 import { MegaMenu } from "primereact/megamenu";
 import { Avatar } from "primereact/avatar";
 import { megaMenuItems } from "@/app/utils/data/MegaMenuItems";
+import { Button } from "primereact/button";
+import { useRouter } from "next/navigation";
 
 export default function NavigationMegaMenu() {
+  const router = useRouter();
+
+  const handleBack = () => {
+    router.back(); // 前のページに戻る
+  };
+
   return (
     <MegaMenu
       model={megaMenuItems}
-      start={<Avatar image="/next.svg" shape="circle" />}
+      start={
+        <>
+          <Button
+            type="button"
+            onClick={handleBack} // ボタンがクリックされたら戻る
+            className="p-button-secondary"
+            // raised
+          >
+            戻る
+          </Button>
+        </>
+      }
       end={<Avatar image="/next.svg" shape="circle" />}
       breakpoint="960px"
     />
