34f674a1d349584562bce5f03de5582108423ee4
山本勇太 <yamamotoyuuta@yamamotoyuutanoMacBook-Pro.local>
2024-12-15 16:22:53 +0900

    fix refactor

--- src/app/(page)/register/page.spec.ts
+++ src/app/(page)/register/page.spec.ts
@@ -1,18 +1,18 @@
 import { test, expect } from "@playwright/test";
 import dotenv from "dotenv";
 dotenv.config();
-const { CREDENTIALS_ID, CREDENTIALS_PASSWORD } = process.env;
+const { URL, CREDENTIALS_ID, CREDENTIALS_PASSWORD } = process.env;
 
 test("register validation", async ({ page }) => {
-  await page.goto("http://localhost:3000/");
+  // 環境変数が定義されているかチェック
+  if (!URL || !CREDENTIALS_ID || !CREDENTIALS_PASSWORD) {
+    throw new Error("環境変数が未定義です");
+  }
+
+  await page.goto(URL);
 
   // 「その他のプロバイダー」リンクをクリック
   await page.getByLabel("その他のプロバイダー").click();
-
-  // 環境変数が定義されているかチェック
-  if (!CREDENTIALS_ID || !CREDENTIALS_PASSWORD) {
-    throw new Error("環境変数が未定義です");
-  }
 
   // ログインフォームの入力
   await page.getByLabel("Id").click();
@@ -25,10 +25,12 @@ test("register validation", async ({ page }) => {
     .getByRole("button", { name: "Sign in with Sample Project" })
     .click();
 
+  await page.waitForURL(URL);
+
   // 「新規会員登録」ラベルの表示を待機
   await page.getByRole("button", { name: "新規会員登録" }).click();
 
-  await page.waitForURL("http://localhost:3000/register");
+  await page.waitForURL(`${URL}/register`);
 
   // フォームの登録ボタンをクリック
   await page.getByRole("button", { name: "登録" }).click();
