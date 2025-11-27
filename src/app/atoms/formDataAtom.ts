1a755a2f40e7b79fc059f1b4221d524b953227f9
Yamamoto Yuta <karate04150422@gmail.com>
2025-11-28 00:13:43 +0900

    update jotai debuglabelを付与

--- src/app/atoms/formDataAtom.ts
+++ src/app/atoms/formDataAtom.ts
@@ -2,3 +2,4 @@ import { atom } from "jotai";
 import { FormValues } from "../components/organisms/navigationMegaMenu/register/stepZero/StepZero";
 
 export const formDataRegisterAtom = atom<FormValues>();
+formDataRegisterAtom.debugLabel = "formDataRegisterAtom";
