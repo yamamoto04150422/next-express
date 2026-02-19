cd40a4096ba8153a8545a14fd910399c706501d0
Yamamoto Yuta <karate04150422@gmail.com>
2026-02-19 22:32:02 +0900

    update #75 axios-qs

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,3 +1,4 @@
+import qs from "qs";
 import { showToast } from "@/app/utils/uiService/toastService";
 import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
 
@@ -5,6 +6,7 @@ export const api = axios.create({
   baseURL: "", // APIのベースURLを設定
   timeout: 10000, // タイムアウト時間を設定（ミリ秒単位）
   withCredentials: true, // クッキーを送信する場合はtrueに設定
+  paramsSerializer: (params) => qs.stringify(params, { arrayFormat: "repeat" }),
 });
 
 // Orval用のmutator関数
